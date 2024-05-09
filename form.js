76c0cbfdd809a83ca02771cb413b8622f972654b
SALA 406D <s406d@umanizales.loc>
2024-05-08 19:52:55 -0500

    Mi primer commit

---
+++ form.js
@@ -0,0 +1,18 @@
+const formulario = document.getElementById("formulario")
+
+formulario.addEventListener("submit", function(){
+    const Username = document.getElementById("Username").value
+    const Email = document.getElementById("Email").value
+    const Password = document.getElementById("Password").value
+    const FullName = document.getElementById("FullName").value
+    const Address = document.getElementById("Address").value
+    const PhoneNumber = document.getElementById("PhoneNumber").value
+
+    console.log("El username es "+Username)
+    console.log("El email es "+Email)
+    console.log("contraseña "+Password)
+    console.log("El nombre es "+FullName)
+    console.log("la direccion es  "+Address)
+    console.log("El numero de telefono es "+PhoneNumber)
+
+})
